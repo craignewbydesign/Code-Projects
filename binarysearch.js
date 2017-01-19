bbbc446108de3bfdcbd83074f59b8475a1358457
craignewbydesign <craignewbydesign@users.noreply.github.com>
2017-01-18 23:55:26 -0500

    Update binarysearch.js

--- binarysearch.js
+++ binarysearch.js
@@ -2,7 +2,8 @@
    
    Javascript implementation of a binary search.
    Returns either the index of the location in the array,
-   or -1 if the array did not contain the targetValue */
+   or -1 if the array did not contain the targetValue 
+*/
 
 
 //Binary Search function
