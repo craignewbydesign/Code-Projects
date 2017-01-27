9ae2b4a4894b8586d9e800baf4240877181e6f43
craignewbydesign <craignewbydesign@users.noreply.github.com>
2017-01-27 18:10:20 -0500

    Add files via upload

---
+++ selectionsort.js
@@ -0,0 +1,57 @@
+/* Selection Sort
+   
+   Javascript implementation of a selection sort using 3 functions.
+   selectionSort()
+   indexOfMinimum()
+   swap()
+*/
+
+// swap minimum value to sort the array correctly 
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+
+// find the minimum value in the unsorted list
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for (var i = minIndex + 1; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    }
+    return minIndex;
+};
+
+// Selection Sort
+var selectionSort = function(array) {
+
+    var minIndex; // holds the index of the current lowest value in array[]
+
+    for (var i = 0; i < array.length - 1; i++) {
+        startIndex = i; //holds the current starting index
+        minIndex = indexOfMinimum(array, startIndex);
+        swap(array, minIndex, startIndex);
+
+    }
+
+
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+// Print Unsorted array[]
+alert("Unsorted list:  " + array);
+
+// Sort Array
+selectionSort(array);
+// Print Selection Sorted array[]
+alert("Selection Sorted list:  " + array);
+
+//println("Array after sorting:  " + array);
+//Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
