c29289fcd1c22091d5917128b6af3cc230e8f75c
craignewbydesign <craignewbydesign@users.noreply.github.com>
2017-02-19 20:54:13 -0500

    Add files via upload

--- insertionSort_es6.js
+++ insertionSort_es6.js
@@ -8,28 +8,40 @@
 //Insertion Sort function
 
 class insertionSort {
-	constructor () {
+	constructor(array) {
+		this.array = array;
+
 	}  
 
-	sort(array){
-	    var len = array.length;
+	sort(){
+	    var len = this.array.length;
 	    for (var i = 0; i < len; i++) {
-	        var tmp = array[i]; //Copy of Current number that needs to be sorted
+	        var tmp = this.array[i]; //Copy of Current number that needs to be sorted
 
 	        //compare tmp with all numbers to the left(already sorted part) of the list of numbers
 	        //If the tmp value is LESS than the number being compared, then shift it to the right.
-	        for (var j = i - 1; j >= 0 && (array[j] > tmp); j--) {
+	        for (var j = i - 1; j >= 0 && (this.array[j] > tmp); j--) {
 	            //Shift the number
-	            array[j + 1] = array[j];
+	            this.array[j + 1] = this.array[j];
 	        }
 	        //If tmp is GREATER than the number being compared, then stop.
 	        //Insert tmp into the correct position 
-	        array[j + 1] = tmp;  
+	        this.array[j + 1] = tmp;  
 	    }
 	}
 }
 
-var array = [5, 3, 1, 2, 4]; 
-let sortedList = new insertionSort();
-sortedList.sort(array);  
+
+var array = [5, 3, 1, 2, 4];  
+var s = new insertionSort(array);
+s.sort();  
 alert(array);  
+
+// Unit test for Insertion Sort
+console.assert(
+  array[0] === 1,
+  array[1] === 2,
+  array[2] === 3,
+  array[3] === 4,
+  array[4] === 5
+);
